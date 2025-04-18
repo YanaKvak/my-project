0113930f6442874aa812d00eb4a4142bc2f148bb
sm1leProgrammer <petaevd@yandex.ru>
2025-04-18 18:10:46 +0300

    init backend

---
+++ newserver/config/cors.js
@@ -0,0 +1,8 @@
+import cors from 'cors';
+
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL || 'http://localhost:3001'],
+  optionsSuccessStatus: 200,
+};
+
+export default cors(corsOptions);
